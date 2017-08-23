9b411b3c48c9b19a7e74016eff81c2deffa2d121
Robster93 <robin-bok@web.de>
2017-08-23 13:27:39 +0200

    works offline

--- script.js
+++ script.js
@@ -2,12 +2,12 @@
 
 function loadNewElement(site) {
 	
-	//if (site == 1) {
+	if (site == "news") {
 		var element = document.getElementById("container");
 		
 		element.innerHTML = '<object type="text/html" data="input.html"></object>'
 		
-		/*var xmlhttp = new XMLHttpRequest();
+		var xmlhttp = new XMLHttpRequest();
 		xmlhttp.onreadystatechange = function() {
 			if (this.readyState == 4 && this.status == 200) {
 				var myObj = JSON.parse(this.responseText);
@@ -17,11 +17,11 @@ function loadNewElement(site) {
 		
 		xmlhttp.open("GET", "data.json", true);
 		xmlhttp.send();
-		*/
-	/*} else if (site == 2) {
+		
+	} else if (site == "contact") {
 		var element = document.getElementById("container");
 		
 		element.innerHTML = '<object type="text/html" data="input.html"></object>'
-	}*/
+	}
 	
 }
