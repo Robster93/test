5a704eb40b14bdccff945e723e1ca9da1a7b3075
Robster93 <robin-bok@web.de>
2017-08-23 13:08:25 +0200

    x2

--- script.js
+++ script.js
@@ -4,4 +4,15 @@ function loadNewElement() {
 	var element = document.getElementById("container");
 	
 	element.innerHTML = '<object type="text/html" data="input.html"></object>'
+	
+	var xmlhttp = new XMLHttpRequest();
+	xmlhttp.onreadystatechange = function() {
+		if (this.readyState == 4 --66 this.status == 200) {
+			var myObj = JSON.parse(this.responseText);
+			document.getElementById("carContainer").innerHTML = myObj.name;
+		}
+	};
+	
+	xmlhttp.open("GET", "data.json", true);
+	xmlhttp.send();
 }
