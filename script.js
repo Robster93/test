ef05d7fa038e47f6137b5be417b45f0c3dc99721
Robster93 <robin-bok@web.de>
2017-08-23 13:16:56 +0200

    new try

--- script.js
+++ script.js
@@ -1,20 +1,27 @@
 
 
-function loadNewElement() {
-	var element = document.getElementById("container");
+function loadNewElement(site) {
 	
-	element.innerHTML = '<object type="text/html" data="input.html"></object>'
-	
-	var xmlhttp = new XMLHttpRequest();
-	xmlhttp.onreadystatechange = function() {
-		if (this.readyState == 4 && this.status == 200) {
-			var myObj = JSON.parse(this.responseText);
-			document.getElementById("carContainer").innerHTML = myObj.name;
-		}
-	};
-	
-	xmlhttp.open("GET", "data.json", true);
-	xmlhttp.send();
-	
+	if (site == "news") {
+		var element = document.getElementById("container");
+		
+		element.innerHTML = '<object type="text/html" data="input.html"></object>'
+		
+		var xmlhttp = new XMLHttpRequest();
+		xmlhttp.onreadystatechange = function() {
+			if (this.readyState == 4 && this.status == 200) {
+				var myObj = JSON.parse(this.responseText);
+				document.getElementById("carContainer").innerHTML = myObj.name;
+			}
+		};
+		
+		xmlhttp.open("GET", "data.json", true);
+		xmlhttp.send();
+		
+	} else if (site == "contact") {
+		var element = document.getElementById("container");
+		
+		element.innerHTML = '<object type="text/html" data="input.html"></object>'
+	}
 	
 }
